b6470061a6a203b4f8f63e78dc16231a30f16f2a
visargashi <visar.gashi@gmail.com>
2016-06-29 00:11:52 -0400

    Implementation of SignalR client started

---
+++ src/device.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using device.client;
+
+namespace device.console
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Starting client connection... ");
+            var client = new Client();
+            client.MessageReceived += Client_MessageReceived;
+            client.Connect().Wait();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Waiting...");
+            Console.ReadLine();
+        }
+
+        private static void Client_MessageReceived(string message)
+        {
+            Console.WriteLine($"From server: {message}");
+        }
+    }
+}
---
+++ src/device.ws/DeviceService.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using System.ServiceProcess;
+
+#endregion
+
+namespace device.ws
+{
+    public partial class DeviceService : ServiceBase
+    {
+        public DeviceService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
