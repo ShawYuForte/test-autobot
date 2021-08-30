a93a8957ed74805b841432f51f62f5b900282633
Arshad Ghafoor <arshad@forte.fit>
2021-08-30 08:56:50 -0400

    Added Windows Events

---
+++ src/forte.modules.serilog/EventLog/LogEventType.cs
@@ -0,0 +1,12 @@
+﻿namespace forte.EventLog
+{
+    public enum LogEventType
+    {
+        Unknown = 0000,
+        Debug = 1001,
+        Information = 2001,
+        Warning = 3001,
+        Error = 4001,
+        Fatal = 5001,
+    }
+}
